3345d000e7ab5dcedf36f74039aaaf30640ca237
AsukaMinato <i@asukaminato.eu.org>
2023-11-11 09:19:28 +0900

    more check

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -3,10 +3,22 @@
 set -e
 
 cargo fmt --check || (
-    echo
-    echo "Please run 'cargo fmt' to format the code."
-    exit 1
+	printf "
+Please run 'cargo fmt' to format the code.
+"
+	exit 1
 )
 
-cargo clippy -- -Dwarnings
+cargo clippy -- -Dwarnings || (
+	printf "
+run 'cargo clippy -- -Dwarnings' to check the code.
+"
+)
+
+cargo clippy -- -D clippy::nursery || (
+	printf "
+run 'cargo clippy -- -D clippy::nursery' to check the code.
+"
+)
+
 cargo test --release
