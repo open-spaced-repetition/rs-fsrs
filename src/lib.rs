135c95ca31efa8891593f1adb08db65736458651
Asuka Minato <i@asukaminato.eu.org>
2024-10-10 03:00:35 +0900

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod scheduler_longterm;
 pub use scheduler_longterm::LongtermScheduler;
 
 mod models;
-pub use models::{Card, Rating, ReviewLog, SchedulingInfo, State, RecordLog};
+pub use models::{Card, Rating, RecordLog, ReviewLog, SchedulingInfo, State};
 
 mod parameters;
 pub use crate::parameters::Parameters;
