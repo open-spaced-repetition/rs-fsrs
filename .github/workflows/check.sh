f5805824b15513f43208e973fd0e6649c6d96aff
Asuka Minato <i@asukaminato.eu.org>
2024-12-01 20:38:01 +0900

    use back stable

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -2,9 +2,7 @@
 
 set -eux -o pipefail
 
-cargo +nightly fmt --check
-
-cargo +nightly clippy -- -D clippy::nursery
+cargo fmt --check
 
 cargo install cargo-llvm-cov --locked
 SKIP_TRAINING=1 cargo llvm-cov --release
