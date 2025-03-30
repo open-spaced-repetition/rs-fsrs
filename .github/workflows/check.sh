a668839bd762b2ce810f358094215b93a7692fa4
Asuka Minato <i@asukaminato.eu.org>
2025-03-30 16:48:58 +0900

    update

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -2,19 +2,5 @@
 
 set -eux -o pipefail
 
-cargo fmt --check || (
-	printf "
-Please run 'cargo fmt' to format the code.
-"
-	exit 1
-)
-
-cargo clippy -- -D clippy::nursery || (
-	printf "
-run 'cargo clippy -- -D clippy::nursery' to check the code.
-"
-	exit 1
-)
-
 cargo install cargo-llvm-cov --locked
 SKIP_TRAINING=1 cargo llvm-cov --release
